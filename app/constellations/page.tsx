6791431ca6b9a6ef182411c646dfae93bef24bd5
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-01-27 19:13:47 -0800

    pulls data successfully with axios

--- app/constellations/page.tsx
+++ app/constellations/page.tsx
@@ -12,42 +12,18 @@ interface Constellation {
 }
 
 const ConstellationsPage = async () => {
+    const res = await axios.get('http://localhost:3000/api/constellations');
+    const constellations: Constellation[] = await res.data;
 
-    // **** CLIENT SIDE RENDERING ****
-    // const [data, setData] = useState<Constellation[]>([])
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await fetch('/api/constellations');
-    //             const result: Constellation[] = await response.json();
-    //             setData(result);
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-
-    //     fetchData();
-    // }, [])
-    // **** CLIENT SIDE RENDERING ****
-
-    // const res = await fetch('/api/constellations')
-    // const constellations: Constellation[] = await res.json();
-    // const res = axios.get('/api/constellations')
     return (
         <main className='flex justify-center items-center flex-col'>
             <div>ConstellationPage</div>
             <Link href="constellations/new" className='p-2 m-5'>New Constellation</Link>
-            {/* {constellations.map(({ id, name, description, distance }: Constellation) => <div key={id}>
+            {constellations.map(({ id, name, description, distance }: Constellation) => <div key={id}>
                 <p>{name}</p>
                 <p>{description}</p>
                 <p>{distance}</p>
-            </div>)} */}
-
-            {/* 
-            CLIENT SIDE RENDERING 
-                {JSON.stringify(data, null, 2)} 
-            CLIENT SIDE RENDERING 
-            */}
+            </div>)}
         </main>
     )
 }
