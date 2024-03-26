99145416b99b3c83306e84c18abbce03ebb9394b
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-03-25 21:53:54 -0700

    scrap

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+'use client'
+import { useSearchParams } from 'next/navigation'
+import React from 'react';
 
-const SearchPage = () => {
+
+
+const SearchPage = async () => {
+
+    const search = useSearchParams();
+    const searchQuery = search ? search.get("q") : null;
+
+    const encodedSearch = encodeURI(searchQuery || '');
+    const response = await fetch(`http://localhost:3000/api/search/${encodedSearch}`, { cache: 'no-store' })
+
+    console.log(response)
+    // console.log("search Params", encodedSearch);
     return (
         <div>Search Page</div>
     )
