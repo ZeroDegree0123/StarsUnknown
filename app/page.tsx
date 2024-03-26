c9189223a5b4ebb36328c001496a1db32f9045fb
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-03-25 21:55:19 -0700

    no mo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import SearchBar from './components/SearchBar/SearchBar'
+
 
 export default function Home() {
 
@@ -7,7 +6,6 @@ export default function Home() {
     <main>
       <section className='flex flex-col justify-center items-center h-screen '>
         <h1 className='text-6xl'>EXPLORE THE UNIVERSE</h1>
-        <SearchBar />
       </section>
       <section>
         <h1>footer</h1>
